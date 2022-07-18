2c74549540a96fc147f5bc67b83305bc6c30b50e
shanthakumarvenkatesh <shanthakumarvenkatesh>
2022-07-18 20:38:49 +0530

    first commit

---
+++ details/Bollywood.js
@@ -0,0 +1,92 @@
+const get = (req,res)=>{
+    res.send([
+        {
+            "id": 1,
+            "title": "RRR 31 Days Box Office Collections",
+            "Image":  "https://www.filmibeat.com/img/2022/04/xb1-1650869738.jpg.pagespeed.ic.sU3Nw2sCwI.jpg",
+            "category": "Bollywood",
+            "description": "Jr NTR and Ram Charan starrer, RRR is refusing to slow down at the box office. The film is holding particularly well in its strong forte - the twin Telugu states Andhra Pradesh and Telangana.",
+     
+          },
+          {
+            "id": 2,
+            "title": "Deepika Padukone EXUDES desi vibes with a modern touch in a white ruffle saree during the closing ceremony",
+            "Image": "https://funroundup.com/wp-content/uploads/2017/09/Deepika-Padukone-Hot-Photos.jpg",
+            "category": "Bollywood",
+            "description": "Gehraiyaan actress Deepika Padukone is among the few stars who are representing India at Cannes 2022. She is churning out some fabulous fashion stints leaving all her fans mesmerised. For the closing ceremony of the international film festival, the actress opted to go all desi. But not without a modern touch.After wearing some classic modern pieces, Deepika Padukone chose a Abu Jani-Sandeep Khosla creation to end her stint at Cannes 2022.The actress looked absolutely divine as walked the red carpet wearing this beautiful saree. The neckpiece made of pearls added much glam to the sweet saree.  The actress attended the press conference with other jury members Rebecca Hall and Jeff Nichols. Sticking to her roots, she greeted everyone with a namaste.Well, if this picture does not hypnotise you then we don't know what will. Subtle makeup, pearl earrings and more - the actress looked drop-dead gorgeous as ever."
+          },
+          {
+            "id": 61,
+            "title": "Deepika Padukone sizzeling desi vibes with a modern touch ",
+            "Image": "https://funroundup.com/wp-content/uploads/2017/09/Deepika-Padukone-Hot-Photos.jpg",
+            "category": "footer1",
+            "description": "Gehraiyaan actress Deepika Padukone is among the few stars who are representing India at Cannes 2022. She is churning out some fabulous fashion stints leaving all her fans mesmerised. For the closing ceremony of the international film festival, the actress opted to go all desi. But not without a modern touch.After wearing some classic modern pieces, Deepika Padukone chose a Abu Jani-Sandeep Khosla creation to end her stint at Cannes 2022.The actress looked absolutely divine as walked the red carpet wearing this beautiful saree. The neckpiece made of pearls added much glam to the sweet saree.  The actress attended the press conference with other jury members Rebecca Hall and Jeff Nichols. Sticking to her roots, she greeted everyone with a namaste.Well, if this picture does not hypnotise you then we don't know what will. Subtle makeup, pearl earrings and more - the actress looked drop-dead gorgeous as ever."
+          },
+          
+          {
+            "id": 3,
+            "title": "RRR movie is wow",
+            "Image": "https://images.hindustantimes.com/img/2021/11/10/1600x900/RRR_1636548612389_1636548617309.PNG",
+            "category": "Bollywood",
+            "description": "Hollywood comedian and actor Patton Oswalt cannot get enough of RRR. The actor who is best known for his Marvel roles including Eternals was left impressed with director SS Rajamouli's latest magnum opus. He recently took to Twitter to recommend the movie.RR was a megahit that had a successful box office run. Apart from breaking records in Indian cinemas, the film also garnered huge praise from international critics and audiences. In a Tweet, Patton Oswalt urged people to watch RRR. He wrote, “If this ISN’T playing near you in IMAX then this is the next best way to watch it. Fucken @RRRMovie is insane.When the team of RRR responded to thank Oswalt, he replied, “You guys are out of your f**king minds, you should not be allowed to make films, and I can’t wait to see what you do next.RR is a period drama starring Ram Charan and NTR Jr and follows two revolutionaries - Alluri Sitarama Raju and Komaram Bheem as they go up against the British rule in India. The film was released on March 24 this year. Since then, it has garnered significant buzz. It also kickstarted a debate on the way pan-India films are perceived."
+          },
+          {
+            "id": 4,
+            "title": "Shah Rukh Khan, Salman Khan and Madhuri Dixit's selfie has fans recalling Hum Tumhare Hai Sanam",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/shahrukhkhan41653712635.jpg",
+            "category": "Bollywood",
+            "description": "Karan Johar's 50th birthday bash was one big star-studded celebration. The evening saw a bevvy of Bollywood celebrities making an appearance and with so many biggies coming together, reunions were inevitable. Now, a selfie of Shah Rukh Khan, Salman Khan and Madhuri Dixit has emerged and fans are obsessed.The picture from the party sees Salman Khan flanked by Madhuri Dixit and Shah Rukh Khan. Joining them were Madhuri's husband Shriram Nene and Shah Rukh Khan's wife Gauri Khan. The Fame Game star took to Instagram to share the priceless photo in a post captioned, “So much to talk about, right? @drneneofficial @iamsrk @beingsalmankhan @gaurikhan. Fans cannot get enough of the picture. One fan commented on the sheer star power and wrote, “All Legends in one frame”. While many were thrilled to get a glimpse of Shah Rukh Khan and Salman Khan together, others recalled that the two stars appeared alongside Madhuri Dixit in Hum Tumhare Hai Sanam. The 2002 film was a romantic drama that revolved around a love triangle. Moreover, the pic also sparked nostalgia for Hum Aapke Hai Koun which saw Madhuri and Salman share screen space in an iconic pairing.Karan Johar’s birthday bash had several memorable moments. While Shah Rukh Khan did not walk the red carpet, videos of him dancing like nobody’s watching on Koi Mil Gaya from Kuch Kuch Hota Hai surfaced later. Meanwhile, Salman Khan who was one of the last guests to arrive reportedly stayed at the party till the morning."
+          },
+          {
+            "id": 5,
+            "title": "Yash Starrer Sails Smooth On Sunday!",
+            "Image": "https://www.filmibeat.com/img/2022/04/xkgf2-1649664764.jpg.pagespeed.ic.8hFQnyy1J6.jpg",
+            "category": "Bollywood",
+            "description" : "Yash's latest film KGF 2 has surely been released after a long delay, but it indeed was worth all the wait and hype, and its super success serves as a testament. The action flick, which is a sequel to his 2018 film KGF, went on to become yet another successful hit from the south belt.Within a time span of 7 days, the film crossed the humongous Rs 700 Crore mark at the global box office."
+          },
+          {
+            "id": 6,
+            "title": "It's official! Hrithik Roshan introduced Saba Azad as his girlfriend at Karan Johar's bash",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/hrithikroshansabaazad41653633774.jpg",
+            "category": "Bollywood",
+            "description": "Talks of Hrithik Roshan and Saba Azad’s rumoured relationship have been doing the rounds for a while. But now, the two have made it red carpet official by appearing together at Karan Johar's grand birthday bash. Hrithik and Saba arrived at the party on theme in their black outfits and made for a stunning pair. Turns out, after gracing the red carpet, Hrithik also introduced Saba Azad as his girlfriend to fellow attendeesAs per reports on a leading news portal, Hrithik Roshan was by Saba Azad's side the whole evening and went on to introduce her as his girlfriend to guests present at the bash. A source told the portal, Hrithik and Saba “didn’t leave each other’s side and were holding hands throughout the party.he couple (now that we can finally call them that) set the red carpet on fire as they posed for paps. Later, actress Preity Zinta also shared a picture with Hrithik and Saba. Take a look at their unmissable picFor the uninitiated, Hrithik Roshan and Saba Azad have been spending a lot of time together and needless to say, they’ve been the talk of the town. While they are yet to make an announcement to confirm their relationship, it cannot get more obvious than this.Recently, Hrithik and Saba also attended a birthday dinner together with the Roshan family. Hrithik’s father Rakesh Roshan and mother Pinkie were present at the intimate family gathering. Meanwhile, Karan Johar’s bash which took place in Mumbai on May 25 was attended by the most popular Bollywood stars including Aishwarya Rai Bachchan and Abhishek Bachchan, Katrina Kaif and Vicky Kaushal, Anushka Sharma, Ranveer Singh, Ranbir Kapoor and others."
+          },
+          {
+            "id": 54,
+            "title": "It's official! Hrithik Roshan introduced Saba Azad as his girlfriend at Karan Johar's bash",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/hrithikroshansabaazad41653633774.jpg",
+            "category": "mix",
+            "description": "Talks of Hrithik Roshan and Saba Azad’s rumoured relationship have been doing the rounds for a while. But now, the two have made it red carpet official by appearing together at Karan Johar's grand birthday bash. Hrithik and Saba arrived at the party on theme in their black outfits and made for a stunning pair. Turns out, after gracing the red carpet, Hrithik also introduced Saba Azad as his girlfriend to fellow attendeesAs per reports on a leading news portal, Hrithik Roshan was by Saba Azad's side the whole evening and went on to introduce her as his girlfriend to guests present at the bash. A source told the portal, Hrithik and Saba “didn’t leave each other’s side and were holding hands throughout the party.he couple (now that we can finally call them that) set the red carpet on fire as they posed for paps. Later, actress Preity Zinta also shared a picture with Hrithik and Saba. Take a look at their unmissable picFor the uninitiated, Hrithik Roshan and Saba Azad have been spending a lot of time together and needless to say, they’ve been the talk of the town. While they are yet to make an announcement to confirm their relationship, it cannot get more obvious than this.Recently, Hrithik and Saba also attended a birthday dinner together with the Roshan family. Hrithik’s father Rakesh Roshan and mother Pinkie were present at the intimate family gathering. Meanwhile, Karan Johar’s bash which took place in Mumbai on May 25 was attended by the most popular Bollywood stars including Aishwarya Rai Bachchan and Abhishek Bachchan, Katrina Kaif and Vicky Kaushal, Anushka Sharma, Ranveer Singh, Ranbir Kapoor and others."
+          },
+          {
+            "id": 7,
+            "title": "Inside pictures from Karan Johar’s 50th birthday: Anushka Sharma and Ranbir Kapoor reunite",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/vickykaushal41653570327.jpg",
+            "category": "Bollywood",
+            "description": "Karan Johar’s 50th birthday bash is the talk of B-town. The event was attended by A-listers such as Salman Khan, Aamir Khan, Kareena Kapoor Khan, Saif Ali Khan, Kajol, Rani Mukerji, Shahid Kapoor, Mira Kapoor, Anushka Sharma, Gauri Khan, Hrithik Roshan, Ranveer Singh, Ranbir Kapoor among others.After the red-carpet pictures of celebrities riled up a storm, we are now looking at the inside pictures from the event. These pictures candidly show our favourite stars having the time of their lives. As Katrina Kaif and Vicky Kaushal dish out couple goals, Ranbir Kapoor and Anushka Sharma reunite. "
+          },
+        {
+          "id": 8,
+          "title": "Bollywood Alia Bhatt finally reacts to viral video of Ranbir Kapoor cradling a baby; cries 'happy tears'",
+          "Image": "https://st1.bollywoodlife.com/wp-content/uploads/2022/05/Alia-Ranbir-baby-video.jpg?impolicy=Medium_Widthonly&w=800",
+          "category": "Bollywood",
+          "description": "A video of Ranbir Kapoor cradling a baby had gone viral on the internet. In the video, Ranbir was seen carrying the little one in his arms, kisses on the head and continues to play with the baby. The fan shared the video on Instagram and wrote, Many social media users started tagging Alia Bhatt after watching the video. And the viral video of Ranbir's has finally got Alia's much-needed attention, and it got her happy tears."
+        },
+        {
+          "id": 9,
+          "title": "Bollywood Deepika Padukone looks jaw droppingly stunning in latest photos",
+          "Image": "https://www.pinkvilla.com/imageresize/deepika_69.jpg?width=752&format=webp&t=pvorg",
+          "category": "Bollywood",
+          "description": "Deepika Padukone never fails to impress us with her style. The stunning actress is an epitome of grace and charm. Deepika is the highest paid actress in the country today and has made it to the Top 5 of Forbes List of Richest Indian Celebs. The actor was featured in TIME magazine’s 100 Most Influential People List and is one of the few Indian actors to have attended coveted events abroad. "
+        },
+        {
+          "id": 10,
+          "title": "Bollywood Aishwarya Rai Bachchan, Abhishek Bachchan and daughter Aaradhya return ",
+          "Image": "https://www.pinkvilla.com/imageresize/abhishek%20bachchan%20aishwarya%20aaradhya%20%282%29.JPG?width=752&format=webp&t=pvorg",
+          "category": "Bollywood",
+          "description": "Aishwarya Rai Bachchan, Abhishek Bachchan and daughter Aaradhya returned to Mumbai today after ringing in 2019 abroad. Abhishek and Aishwarya shared photos from their New Year getaway as the family basked in the sun. At the airport, daughter Aaradhya was engaged in a conversation with her parents and looked cute as a button in a pink dress."
+        }
+    ])
+}
+
+module.exports.apiController = get;
--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-// const express =require("express")
-// const app=express()
-// const jsonData=require("./data")
+const express = require('express')
+const bollyRouter = require('./route/Bollywood')
+const hollyRouter = require('./route/Hollywood')
+const techRouter = require('./route/Technology')
+const fitRouter = require('./route/Fitness')
+const foodRouter = require('./route/Food')
+const homeRouter = require('./route/Home')
+const cors = require("cors")
+const app = express()
 
-// app.get('/',function(req,res){
-//     res.send("<h1>shanth</h1>")
-// })
-// app.get('/user1',function(req,res){
-//     res.json(jsonData.json1)
-//     console.log(jsonData.add(3,4))
-// })
-// app.get('/user2',function(req,res){
-//     res.json(jsonData.json2)
-  
-// })
-// app.listen(3001)
+app.use(cors())
+app.use("/api",bollyRouter)
+app.use("/api",hollyRouter)
+app.use("/api",techRouter)
+app.use("/api",fitRouter)
+app.use("/api",foodRouter)
+app.use("/api",homeRouter)
 
-const express=require("express");
 
-const app=express();
-
-app.get("/",(req,res)=>
-{
-    res.send("hello express");
-});
-
-app.get("/api/main",(req,res)=>{
-    res.write("<h1>Exxpress.js</h1>");
-    res.write("<p>Express is a node js web application framwork that provides us broad features for web and mobile applications.It is a layer built on the top of the node js that helps manages servers and routes.</p>");
-    res.end();
-});
-app.listen(8005);
+app.listen(process.env.PORT||8000,()=>{
+    console.log("app is running");
+})
